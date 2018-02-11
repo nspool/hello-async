bed2f1fe40ba32eb33a9b4c8d5a8c11cf3f7206c
nspool <nspool@gmx.com>
2018-02-11 16:49:29 +1100

    initial commit

---
+++ AsyncConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncConsole
+{
+    class Program
+    {
+        static async Task DoSomethingAsync()
+        {
+            int val = 13;
+            await Task.Delay(TimeSpan.FromSeconds(1));
+
+            val *= 2;
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+
+            Trace.WriteLine(val);
+        }
+
+        static void Main(string[] args)
+        {
+            Task.Run(async () => { await DoSomethingAsync(); });
+        }
+    }
+}
