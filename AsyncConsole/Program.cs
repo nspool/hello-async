9b26408d806cae5469a3adb08f3645c331d9786a
nspool <nspool@gmx.com>
2018-02-11 22:14:35 +1100

    Add Rx example

--- AsyncConsole/Program.cs
+++ AsyncConsole/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reactive.Subjects;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,21 +10,54 @@ namespace AsyncConsole
 {
     class Program
     {
+        /// <summary>
+        /// This class demonstrates await.
+        /// </summary>
         static async Task DoSomethingAsync()
         {
-            int val = 13;
-            await Task.Delay(TimeSpan.FromSeconds(1));
+            int val = 10;
+
+            await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
+
+            Trace.WriteLine(val.ToString());
 
             val *= 2;
 
-            await Task.Delay(TimeSpan.FromSeconds(1));
+            await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
 
-            Trace.WriteLine(val);
+            Trace.WriteLine(val.ToString());
         }
 
+
+        /// <summary>
+        /// Takes an IObservable<string> as its parameter. 
+        /// Subject<string> implements this interface.
+        /// (from http://www.introtorx.com/Content/v1.0.10621.0/03_LifetimeManagement.html)
+        /// </summary>
+        static void WriteSequenceToConsole(IObservable<int> sequence)
+        {
+            // sequence.Subscribe(Console.WriteLine);
+            sequence.Subscribe(value => Trace.WriteLine(value));
+
+        }
+
+        /// <summary>
+        /// Entry point for the program
+        /// </summary>
         static void Main(string[] args)
         {
-            Task.Run(async () => { await DoSomethingAsync(); });
+            Task doSomethingTask = DoSomethingAsync();
+
+            // Block synchronously
+            doSomethingTask.Wait();
+
+            var subject = new Subject<int>();
+            WriteSequenceToConsole(subject);
+            subject.OnNext(1);
+            subject.OnNext(2);
+            subject.OnNext(3);
         }
+
+
     }
 }
